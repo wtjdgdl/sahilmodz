0a91f4b2b9d3f6e2c62819d72094fa94449d2266
SAHILMOD10 <githubsahil@vsmailpro.com>
2025-04-08 23:09:59 +0530

    Update sahil.py

--- sahil.py
+++ sahil.py
@@ -11,7 +11,7 @@ from threading import Thread
 loop = asyncio.get_event_loop()
 
 # Bot token
-TOKEN = '7362289727:AAE6RWfho65aJUxMyOSdvxXVBndKGx4QZcc'
+TOKEN = '7252746907:AAGkWBL1YkjwKX_ceXzNW-xxVuPLuRoEME0'
 bot = telebot.TeleBot(TOKEN)
 REQUEST_INTERVAL = 1
 
